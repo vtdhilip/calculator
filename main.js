41593623725692f3f6e4ae691a1da61cc9c2ce0c
Dhilipvt <cartoontamilzan@gmail.com>
2022-11-20 14:20:53 +0530

    feat: ui design

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
+
+setupCounter(document.querySelector('#counter'))
---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brcolor': '#ff5a00'
+       },
+    },
+  },
+  plugins: [],
+}
